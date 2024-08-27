8ee78fce59376bf9e1e7046b1dba32d4ac5c767a
Mathijs Bernson <mathijsb@q42.nl>
2024-08-27 17:31:22 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,14 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-  name: "Locksmith"
+    name: "Locksmith",
+    products: [
+        .library(name: "Locksmith", targets: ["Locksmith"]),
+    ],
+    targets: [
+        .target(name: "Locksmith", path: "Source"),
+    ]
 )
